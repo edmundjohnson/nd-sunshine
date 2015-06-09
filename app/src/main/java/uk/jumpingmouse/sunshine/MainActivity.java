9eb4cd37a86b45f3eaf926ab4a23a8572f54f580
Edmund Johnson <edmund.johnson@rexel.co.uk>
2015-06-09 17:43:00 +0100

    Move fragment into MainActivity

--- app/src/main/java/uk/jumpingmouse/sunshine/MainActivity.java
+++ app/src/main/java/uk/jumpingmouse/sunshine/MainActivity.java
@@ -1,9 +1,13 @@
 package uk.jumpingmouse.sunshine;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -36,4 +40,20 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_main, container, false);
+        }
+    }
+
 }
