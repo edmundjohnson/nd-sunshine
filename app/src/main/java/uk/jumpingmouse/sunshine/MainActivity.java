99e7cc4ecea042331b1d16dc85fa46045bce3b31
Edmund Johnson <edmund.johnson@rexel.co.uk>
2015-06-10 20:24:00 +0100

    Complete lesson 1

--- app/src/main/java/uk/jumpingmouse/sunshine/MainActivity.java
+++ app/src/main/java/uk/jumpingmouse/sunshine/MainActivity.java
@@ -1,15 +1,15 @@
 package uk.jumpingmouse.sunshine;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 
+/**
+ * The main activity.
+ * @author Edmund Johnson.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -39,21 +39,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_main, container, false);
-        }
     }
 
 }
